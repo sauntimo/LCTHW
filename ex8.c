5077e84a740964dad1e98f089b97d33b6f686558
sauntimo <tim@sauntimo.org>
2014-11-17 23:36:22 +0000

    edits to ex8

--- ex8.c
+++ ex8.c
@@ -4,9 +4,7 @@ int main(int agrc, char *argv[])
 {
 	int areas[] = {20, 12, 13, 14, 20};
 	char name[] = "Tim";
-	char full_name = {
-		'T','i','m',' ','S','a','u','n','d','e','r','s','\0'
-	};
+	char full_name[] = {'T','i','m',' ','S','a','u','n','d','e','r','s','\0'};
 	
 	/* WARNING: on some systems you may have to change the 
 	%ld in this code to a %u since is wil use unsigned ints */
@@ -15,15 +13,15 @@ int main(int agrc, char *argv[])
 	
 	printf("The size of areas (int[]): %ld\n", sizeof(areas));
 	
-	printf("The first area is %d, the 2nd is %d.\n, areas[0], areas[1]);
+	printf("The first area is %d, the 2nd is %d.\n", areas[0], areas[1]);
 	
 	printf("The size of a char: %ld\n", sizeof(char));
 	
-	print("The size of name (char[]): %ld\n", sizeof(name));
+	printf("The size of name (char[]): %ld\n", sizeof(name));
 	
-	printf("The numbers of chars: %ld\n", sizeof(name) / sizeof(char);
+	printf("The numbers of chars: %ld\n", sizeof(name) / sizeof(char));
 	
-	print("The size of full_name (char[]): %ld\n", sizeof(full_name));
+	printf("The size of full_name (char[]): %ld\n", sizeof(full_name));
 	
 	printf("The number of chars: %ld\n", sizeof(full_name) / sizeof(char)); 
 	
